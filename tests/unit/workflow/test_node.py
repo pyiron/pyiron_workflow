bdde1514fac7a3d2ba001479146485dccc881e92
liamhuber <liamhuber@greyhavensolutions.com>
2023-02-03 11:09:46 -0800

    Update tests

--- tests/unit/workflow/test_node.py
+++ tests/unit/workflow/test_node.py
@@ -1,3 +1,79 @@
 from unittest import TestCase
 
-from pyiron_contrib.workflow.node import Node
+from pyiron_contrib.workflow.node import Node, pass_all
+from pyiron_contrib.workflow.channels import ChannelTemplate
+
+
+def throw_error(x=None):
+    raise RuntimeError
+
+
+def plus_one(x=None):
+    return {'y': x + 1}
+
+
+class ChildNode(Node):
+    """
+    All sub-compnents are defined, but __init__ is not overriden -- should throw an
+    error every time we give non-None input for these!
+    """
+    input_channels = [ChannelTemplate(name='x', types=(int))]
+    preprocessor = staticmethod(pass_all)
+    engine = staticmethod(plus_one)
+    # postprocessor = staticmethod(pass_all)  # We could define it as a property instead
+    output_channels = [ChannelTemplate(name='y')]
+
+    @staticmethod
+    def postprocessor(**kwargs):
+        return pass_all(**kwargs)
+
+
+class TestNode(TestCase):
+    def test_defaults(self):
+        node = Node("a_node")
+
+    def test_instantiation_update(self):
+        no_update = Node(
+            "has_no_output",
+            engine=plus_one,
+            input_channels=[ChannelTemplate(name='x', default=1)],
+            output_channels=[ChannelTemplate(name='y')],
+            update_now=False
+        )
+        self.assertIsNone(no_update.output.y.value)
+
+        update = Node(
+            "has_no_output",
+            engine=plus_one,
+            input_channels=[ChannelTemplate(name='x', default=1)],
+            output_channels=[ChannelTemplate(name='y')],
+            update_now=True
+        )
+        self.assertEqual(2, update.output.y.value)
+
+    def test_automatic_updates(self):
+        node = Node(
+            "death_node",
+            engine=throw_error,
+            input_channels=[ChannelTemplate(name='x', types=int)],
+        )
+
+        with self.subTest("Shouldn't run for invalid input on update"):
+            node.input.x.update("not an int")
+
+        with self.subTest("Valid data should trigger a run"):
+            with self.assertRaises(RuntimeError):
+                node.input.x.update(1)
+
+    def test_double_definitions(self):
+        child = ChildNode("child")
+
+        for kwargs in [
+            {"input_channels": ChildNode.input_channels},
+            {"preprocessor": ChildNode.preprocessor},
+            {"engine": ChildNode.engine},
+            {"postprocessor": ChildNode.postprocessor},
+            {"output_channels": ChildNode.output_channels},
+        ]:
+            with self.assertRaises(ValueError):
+                ChildNode("input_tries_to_override_class", **kwargs)
