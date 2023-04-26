b4902732ba18034dcb492860c8ab6ab5b856f5d2
liamhuber <liamhuber@greyhavensolutions.com>
2023-04-26 14:10:38 -0700

    Expose decorator on the workflow

--- pyiron_contrib/workflow/workflow.py
+++ pyiron_contrib/workflow/workflow.py
@@ -4,7 +4,7 @@ from functools import partial
 from warnings import warn
 
 from pyiron_contrib.workflow.has_to_dict import HasToDict
-from pyiron_contrib.workflow.node import Node, node
+from pyiron_contrib.workflow.node import Node, node, fast_node
 from pyiron_contrib.workflow.util import DotDict
 
 
@@ -63,6 +63,7 @@ class _NodeAdder:
 class _NodeDecoratorAccess:
     """An intermediate container to store node-creating decorators as class methods."""
     node = node
+    fast_node = fast_node
 
 
 class Workflow(HasToDict):
