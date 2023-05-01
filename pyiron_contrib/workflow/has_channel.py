9d5a97ba500a2813ca1e83a38c747b7dd2d0aa9e
liamhuber <liamhuber@greyhavensolutions.com>
2023-05-01 14:35:23 -0700

    Import from annotations to get the type hint working

--- pyiron_contrib/workflow/has_channel.py
+++ pyiron_contrib/workflow/has_channel.py
@@ -2,6 +2,8 @@
 # Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
