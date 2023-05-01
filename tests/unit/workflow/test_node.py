024ab5737725ead7e25fbb19b5cd5ffb84d2f73d
liamhuber <liamhuber@greyhavensolutions.com>
2023-05-01 12:36:53 -0700

    Fix the string test
    "In" is insufficiently specific -- in this case the numeric output from the node could easily appear in the memory address shown during the default string representation.

--- tests/unit/workflow/test_node.py
+++ tests/unit/workflow/test_node.py
@@ -177,8 +177,8 @@ class TestSingleValueNode(TestCase):
 
     def test_str(self):
         svn = SingleValueNode(plus_one, "y")
-        self.assertIn(
-            str(svn.outputs.y.value), str(svn),
+        self.assertTrue(
+            str(svn).endswith(str(svn.single_value)),
             msg="SingleValueNodes should have their output as a string in their string "
                 "representation (e.g., perhaps with a reminder note that this is "
                 "actually still a Node and not just the value you're seeing.)"
