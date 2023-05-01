338301cd5236f0e7ddcc0fdbe510a302ef28514b
liamhuber <liamhuber@greyhavensolutions.com>
2023-05-01 14:24:39 -0700

    Introduce a new mix-in for connections

---
+++ pyiron_contrib/workflow/has_connection.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pyiron_contrib.workflow.channels import Channel
+
+
+class HasConnection(ABC):
+    """
+    A mix-in class for use with the `Channel` class.
+    A `Channel` is able to (attempt to) connect to any child instance of `HasConnection`
+    by looking at its `connection` attribute.
+
+    This is useful for letting channels attempt to connect to non-channel objects
+    directly by pointing them to some channel that object holds.
+    """
+    @property
+    @abstractmethod
+    def connection(self) -> Channel:
+        pass
