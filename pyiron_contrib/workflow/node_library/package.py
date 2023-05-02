d7013f50d9bfc08332fe78cc1ba9bf2fb5b88a14
liamhuber <liamhuber@greyhavensolutions.com>
2023-05-02 10:41:05 -0700

    Introduce a new class for holding collections of nodes

---
+++ pyiron_contrib/workflow/node_library/package.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from functools import partial
+from typing import TYPE_CHECKING
+
+from pyiron_contrib.workflow.node import Node
+from pyiron_contrib.workflow.util import DotDict
+
+if TYPE_CHECKING:
+    from pyiron_contrib.workflow.workflow import Workflow
+
+
+class NodePackage(DotDict):
+    """
+    A collection of node classes that, when instantiated, will have their workflow
+    automatically set.
+
+    Node classes are accessible by their _class name_ by item or attribute access.
+
+    Can be extended by adding node classes to new names with an item or attribute set,
+    but to update an existing node the `update` method must be used.
+    """
+
+    def __init__(self, workflow: Workflow, *node_classes: Node):
+        super().__init__()
+        self.__dict__['_workflow'] = workflow  # Avoid the __setattr__ override
+        for node in node_classes:
+            self[node.__name__] = node
+
+    def __setitem__(self, key, value):
+        if key in self.keys():
+            raise KeyError(f"The name {key} is already a stored node class.")
+        elif key in self.__dir__():
+            raise KeyError(
+                f"The name {key} is already an attribute of this "
+                f"{self.__class__.__name__} instance."
+            )
+        if not isinstance(value, type) or not issubclass(value, Node):
+            raise TypeError(
+                f"Can only set members that are (sub)classes of  {Node.__name__}, but "
+                f"got {type(value)}"
+            )
+        super().__setitem__(key, value)
+
+    def __getitem__(self, item):
+        value = super().__getitem__(item)
+        if issubclass(value, Node):
+            return partial(value, workflow=self._workflow)
+        else:
+            return value
+
+    def update(self, *node_classes):
+        replacing = set(self.keys()).intersection([n.__name__ for n in node_classes])
+        for name in replacing:
+            del self[name]
+
+        for node in node_classes:
+            self[node.__name__] = node
---
+++ tests/unit/workflow/test_node_package.py
@@ -0,0 +1,71 @@
+from unittest import TestCase, skipUnless
+from sys import version_info
+
+from pyiron_contrib.workflow.node_library.package import NodePackage
+from pyiron_contrib.workflow.workflow import Workflow
+
+
+@Workflow.wrap_as.fast_node("x")
+def dummy(x: int = 0):
+    return x
+
+
+@skipUnless(version_info[0] == 3 and version_info[1] >= 10, "Only supported for 3.10+")
+class TestNodePackage(TestCase):
+    def setUp(self) -> None:
+        self.wf = Workflow("test_workflow")
+        self.package = NodePackage(self.wf, dummy)
+
+    def test_init(self):
+        self.assertTrue(
+            hasattr(self.package, dummy.__name__),
+            msg="Classes should be added at instantiation"
+        )
+
+    def test_access(self):
+        node = self.package.Dummy()
+        self.assertIsInstance(node, dummy)
+        self.assertIs(
+            node.workflow,
+            self.package._workflow,
+            msg="Package workflow should get assigned to node instances"
+        )
+
+    def test_update(self):
+        with self.assertRaises(KeyError):
+            self.package.Dummy = "This is already a node class name"
+
+        with self.assertRaises(KeyError):
+            self.package.update = "This is already a method"
+
+        with self.assertRaises(TypeError):
+            self.package.available_name = "But we can still only assign node classes"
+
+        @Workflow.wrap_as.node("y")
+        def add(x: int = 0):
+            return x + 1
+
+        self.package.node_class_and_free_key = add  # Should work!
+
+        with self.assertRaises(KeyError):
+            # This is already occupied by another node class
+            self.package.Dummy = add
+
+        old_dummy_instance = self.package.Dummy(label="old_dummy_instance")
+
+        @Workflow.wrap_as.fast_node("y")
+        def dummy(x: int = 0):
+            return x + 1
+
+        self.package.update(dummy)
+
+        self.assertEqual(len(self.package), 2, msg="Update should replace, not extend")
+
+        new_dummy_instance = self.package.Dummy(label="new_dummy_instance")
+
+        self.assertEqual(
+            old_dummy_instance.outputs.x.value, 0, msg="Should have old functionality"
+        )
+        self.assertEqual(
+            new_dummy_instance.outputs.y.value, 1, msg="Should have new functionality"
+        )
