5c9f8891e268d3f4bdff3f843208c535e5130b89
liamhuber <liamhuber@greyhavensolutions.com>
2023-06-13 11:29:00 -0700

    Introduce meta nodes
    callables that create node classes following a pattern

---
+++ pyiron_contrib/workflow/meta.py
@@ -0,0 +1,27 @@
+"""
+Meta nodes are callables that create a node class instead of a node instance.
+"""
+
+from __future__ import annotations
+
+from pyiron_contrib.workflow.node import single_value_node, SingleValueNode
+
+
+def _input_to_list(n_elements) -> callable:
+    string = "def input_to_list("
+    for i in range(n_elements):
+        string += f"i{i}=None, "
+    string += "): return ["
+    for i in range(n_elements):
+        string += f"i{i}, "
+    string += "]"
+    exec(string)
+    return locals()["input_to_list"]
+
+
+class MetaNodes:
+    """A container class for meta node access"""
+
+    @classmethod
+    def input_to_list(cls, n_inputs: int) -> SingleValueNode:
+        return single_value_node("list")(_input_to_list(n_inputs))
