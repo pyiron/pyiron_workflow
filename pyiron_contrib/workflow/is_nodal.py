4de6c24fb6e1b1d5661fb83e09db814025fa262b
liamhuber <liamhuber@greyhavensolutions.com>
2023-06-22 11:26:08 -0700

    Add docstrings

--- pyiron_contrib/workflow/is_nodal.py
+++ pyiron_contrib/workflow/is_nodal.py
@@ -18,10 +18,65 @@ if TYPE_CHECKING:
 
 class IsNodal(ABC):
     """
-    A mixin class for classes that can represent nodes on a computation graph.
+    A mixin class for objects that can form nodes in the graph representation of a
+    computational workflow.
+
+    Nodal objects have `inputs` and `outputs` channels for passing data, and `signals`
+    channels for making callbacks on the class (input) and controlling execution flow
+    (output) when connected to other nodal objects.
+
+    Nodal objects can `run` to complete some computational task, or call a softer
+    `update` which will run the task only if it is `ready` -- i.e. it is not currently
+    running, has not previously tried to run and failed, and all of its inputs are ready
+    (i.e. populated with data that passes type requirements, if any).
+
+    Attributes:
+        connected (bool): Whether _any_ of the IO (including signals) are connected.
+        failed (bool): Whether the nodal object raised an error calling `run`. (Default
+            is False.)
+        fully_connected (bool): whether _all_ of the IO (including signals) are
+            connected.
+        inputs (pyiron_contrib.workflow.io.Inputs): **Abstract.** Children must define
+            a property returning an `Inputs` object.
+        label (str): A name for the nodal object.
+        output (pyiron_contrib.workflow.io.Outputs): **Abstract.** Children must define
+            a property returning an `Outputs` object.
+        ready (bool): Whether the inputs are all ready and the nodal object is neither
+            already running nor already failed.
+        running (bool): Whether the nodal object has called `run` and has not yet
+            received output from from this call. (Default is False.)
+        server (Optional[pyiron_base.jobs.job.extension.server.generic.Server]): A
+            server object for computing things somewhere else. Default (and currently
+            _only_) behaviour is to compute things on the main python process owning
+            the nodal object.
+        signals (pyiron_contrib.workflow.io.Signals): A container for input and output
+            signals, which are channels for controlling execution flow. By default, has
+            a `signals.inputs.run` channel which has a callback to the `run` method,
+            and `signals.outputs.ran` which should be called at when the `run` method
+            is finished (TODO: Don't leave this step up to child class developers!).
+            Additional signal channels in derived classes can be added to
+            `signals.inputs` and  `signals.outputs` after this mixin class is
+            initialized.
+
+    Methods:
+        disconnect: Remove all connections, including signals.
+        run: **Abstract.** Do the thing.
+        update: **Abstract.** Do the thing if you're ready and you run on updates.
+            TODO: Once `run_on_updates` is in this class, we can un-abstract this.
     """
 
     def __init__(self, label: str, *args, **kwargs):
+        """
+        A mixin class for objects that can form nodes in the graph representation of a
+        computational workflow.
+
+        Args:
+            label (str): A name for this nodal object.
+            *args: Arguments passed on with `super`.
+            **kwargs: Keyword arguments passed on with `super`.
+
+        TODO: Shouldn't `update_on_instantiation` and `run_on_updates` both live here??
+        """
         super().__init__(*args, **kwargs)
         self.label: str = label
         self.running = False
@@ -30,6 +85,8 @@ class IsNodal(ABC):
         self._server: Server | None = (
             None  # Or "task_manager" or "executor" -- we'll see what's best
         )
+        # TODO: Move from a traditional "sever" to a tinybase "executor"
+        # TODO: Provide support for actually computing stuff with the server/executor
         self.signals = self._build_signal_channels()
 
     @property
@@ -54,6 +111,10 @@ class IsNodal(ABC):
         signals = Signals()
         signals.input.run = InputSignal("run", self, self.run)
         signals.output.ran = OutputSignal("ran", self)
+        # TODO: Build `run` such that developers inheriting from this class don't need
+        #       to remember to invoke `self.signals.output.ran()`! Probably this will
+        #       involve pulling `run` up into this class and exposing `_on_run` as an
+        #       abstract method to developers (or a similar attack).
         return signals
 
     @property
