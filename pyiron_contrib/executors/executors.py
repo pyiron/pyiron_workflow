ef17d5d88818268e1c5a61a3a6d4f1e1cf18ce31
liamhuber <liamhuber@greyhavensolutions.com>
2023-07-11 13:54:36 -0700

    Be more specific about limitations

--- pyiron_contrib/executors/executors.py
+++ pyiron_contrib/executors/executors.py
@@ -10,13 +10,17 @@ def _apply_cloudpickle(fn, /, *args, **kwargs):
 
 class CloudpickleProcessPoolExecutor(ProcessPoolExecutor):
     """
-    This wrapper class replaces the `pickle` backend of the
-    `concurrent.futures.ProcessPoolExecutor` with a backend from `cloudpickle`.
-    In this way, even objects with no canonical import (e.g. those created dynamically
-    from a decorator) can be submitted to the executor.
+    This executor behaves like `concurrent.futures.ProcessPoolExecutor`, except that
+    non-pickleable callables may also be submit (e.g. dynamically defined functions).
+
+    This is accomplished by replacing the `pickle` backend of the
+    `concurrent.futures.ProcessPoolExecutor` with a backend from `cloudpickle` when
+    serializing the callable.
 
     This solution comes from u/mostsquares @ stackoverflow:
     https://stackoverflow.com/questions/62830970/submit-dynamically-loaded-functions-to-the-processpoolexecutor
+
+    Note: Arguments and return values must still be regularly pickleable.
     """
 
     def submit(self, fn, /, *args, **kwargs):
