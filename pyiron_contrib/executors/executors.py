f698f85db32736d4d38ff741c261a2aa51725d1d
liamhuber <liamhuber@greyhavensolutions.com>
2023-07-11 15:04:00 -0700

    Draft a universally-cloud-pickling process pool executor

--- pyiron_contrib/executors/executors.py
+++ pyiron_contrib/executors/executors.py
@@ -1,4 +1,5 @@
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import Future, ProcessPoolExecutor
+from concurrent.futures.process import _global_shutdown, _WorkItem, BrokenProcessPool
 
 import cloudpickle
 
@@ -27,3 +28,59 @@ class CloudpickleProcessPoolExecutor(ProcessPoolExecutor):
         return super().submit(
             _apply_cloudpickle, cloudpickle.dumps(fn), *args, **kwargs
         )
+
+
+class CloudLoadsFuture(Future):
+    def result(self, timeout=None):
+        result = super().result(timeout=timeout)
+        if isinstance(result, bytes):
+            result = cloudpickle.loads(result)
+        return result
+
+
+class CloudPickledCallable:
+    def __init__(self, fnc: callable):
+        self.fnc_serial = cloudpickle.dumps(fnc)
+
+    def __call__(self, /, dumped_args, dumped_kwargs):
+        fnc = cloudpickle.loads(self.fnc_serial)
+        args = cloudpickle.loads(dumped_args)
+        kwargs = cloudpickle.loads(dumped_kwargs)
+        return cloudpickle.dumps(fnc(*args, **kwargs))
+
+    @classmethod
+    def dumps(cls, stuff):
+        return cloudpickle.dumps(stuff)
+
+
+class CloudProcessPoolExecutor(ProcessPoolExecutor):
+    def submit(self, fn, /, *args, **kwargs):
+        return self._submit(
+            CloudPickledCallable(fn),
+            CloudPickledCallable.dumps(args),
+            CloudPickledCallable.dumps(kwargs)
+        )
+
+    def _submit(self, fn, /, *args, **kwargs):
+        with self._shutdown_lock:
+            if self._broken:
+                raise BrokenProcessPool(self._broken)
+            if self._shutdown_thread:
+                raise RuntimeError('cannot schedule new futures after shutdown')
+            if _global_shutdown:
+                raise RuntimeError('cannot schedule new futures after '
+                                   'interpreter shutdown')
+
+            f = CloudLoadsFuture()
+            w = _WorkItem(f, fn, args, kwargs)
+
+            self._pending_work_items[self._queue_count] = w
+            self._work_ids.put(self._queue_count)
+            self._queue_count += 1
+            # Wake up queue management thread
+            self._executor_manager_thread_wakeup.wakeup()
+
+            if self._safe_to_dynamically_spawn_children:
+                self._adjust_process_count()
+            self._start_executor_manager_thread()
+            return f
