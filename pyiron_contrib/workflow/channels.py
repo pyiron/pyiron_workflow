8ad76ac396c05c15a58e911229287d98053ca635
liamhuber <liamhuber@greyhavensolutions.com>
2023-09-14 10:07:06 -0700

    Add warning per @samwaseda's advice

--- pyiron_contrib/workflow/channels.py
+++ pyiron_contrib/workflow/channels.py
@@ -106,6 +106,11 @@ class Channel(HasChannel, HasToDict, ABC):
                 self.connections.remove(other)
                 other.disconnect(self)
                 destroyed_connections.append((self, other))
+            else:
+                warn(
+                    f"The channel {self.label} was not connected to {other.label}, and"
+                    f"thus could not disconnect from it."
+                )
         return destroyed_connections
 
     def disconnect_all(self) -> list[tuple[Channel, Channel]]:
