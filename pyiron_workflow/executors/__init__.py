3980e0742a0e555f9da3cfae4d366b9cfd9d2403
liamhuber <liamhuber@greyhavensolutions.com>
2023-10-06 11:17:12 -0700

    Sue correct path

--- pyiron_workflow/executors/__init__.py
+++ pyiron_workflow/executors/__init__.py
@@ -2,6 +2,6 @@
 This module holds customized children of `concurrent.futures.Executor`.
 """
 
-from pyiron_contrib.executors.cloudpickleprocesspool import (
+from pyiron_workflow.executors.cloudpickleprocesspool import (
     CloudpickleProcessPoolExecutor,
 )
