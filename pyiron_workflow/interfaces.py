792fa09b8d6eff7efa700c9804dc630b34f17750
pyiron-runner <pyiron@mpie.de>
2023-10-06 20:57:51 +0000

    Format black

--- pyiron_workflow/interfaces.py
+++ pyiron_workflow/interfaces.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from pyiron_base.interfaces.singleton import Singleton
+
 # from pyiron_contrib.executors import CloudpickleProcessPoolExecutor as Executor
 # from pympipool.mpi.executor import PyMPISingleTaskExecutor as Executor
 
