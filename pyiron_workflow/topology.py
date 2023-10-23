abf6c5cb352c19039f1f171da1caf880754bd877
liamhuber <liamhuber@greyhavensolutions.com>
2023-10-23 13:09:09 -0700

    Use the dictionary of nodes instead of list
    Which lets us be a little more robust checking for inconsistencies

--- pyiron_workflow/topology.py
+++ pyiron_workflow/topology.py
@@ -15,10 +15,14 @@ if TYPE_CHECKING:
     from pyiron_workflow.node import Node
 
 
-def nodes_to_data_digraph(*nodes: Node) -> dict[str, set[str]]:
+def nodes_to_data_digraph(nodes: dict[str, Node]) -> dict[str, set[str]]:
     """
     Maps a set of nodes to a digraph of their data dependency in the format of label
     keys and set of label values for upstream nodes.
+
+    Args:
+        nodes (dict[str, Node]): A label-keyed dictionary of nodes to convert into a
+            string-based dictionary of digraph connections based on data flow.
 
     Returns:
         dict[str, set[str]]: A dictionary of nodes and the nodes they depend on for
@@ -32,28 +36,35 @@ def nodes_to_data_digraph(*nodes: Node) -> dict[str, set[str]]:
     """
     digraph = {}
 
-    parent = nodes[0].parent
-    if not all(n.parent is parent for n in nodes):
+    parent = next(iter(nodes.values())).parent  # Just grab any one
+    if not all(n.parent is parent for n in nodes.values()):
         raise ValueError(
             "Nodes in a data digraph must all be siblings -- i.e. have the same "
             "`parent` attribute."
         )
 
-    for node in nodes:
+    for node in nodes.values():
         node_dependencies = []
         for channel in node.inputs:
             locally_scoped_dependencies = []
             for upstream in channel.connections:
-                if upstream.node.parent is parent:
-                    locally_scoped_dependencies.append(upstream.node.label)
-                else:
+                try:
+                    upstream_node = nodes[upstream.node.label]
+                except KeyError as e:
+                    raise KeyError(
+                        f"The {channel.label} channel of {node.label} has a connection "
+                        f"to {upstream.label} channel of {upstream.node.label}, but "
+                        f"{upstream.node.label} was not found among nodes. All nodes "
+                        f"in the data flow dependency tree must be included.")
+                if upstream_node is not upstream.node:
                     raise ValueError(
-                        f"Nodes in a data digraph must all be siblings, but the "
-                        f"{channel.label} channel of {node.label} has a connection to "
-                        f"the {upstream.label} channel of {upstream.node.label} with "
-                        f"parents {node.parent} and {upstream.node.parent}, "
-                        f"respectively"
+                        f"The {channel.label} channel of {node.label} has a connection "
+                        f"to {upstream.label} channel of {upstream.node.label}, but "
+                        f"that channel's node is not the same as the nodes passed "
+                        f"here. All nodes in the data flow dependency tree must be "
+                        f"included."
                     )
+                locally_scoped_dependencies.append(upstream.node.label)
             node_dependencies.extend(locally_scoped_dependencies)
         node_dependencies = set(node_dependencies)
         if node.label in node_dependencies:
@@ -69,14 +80,15 @@ def nodes_to_data_digraph(*nodes: Node) -> dict[str, set[str]]:
     return digraph
 
 
-def nodes_to_execution_order(*nodes: Node) -> list[str]:
+def nodes_to_execution_order(nodes: dict[str, Node]) -> list[str]:
     """
     Given a set of nodes that all have the same parent, returns a list of corresponding
     node labels giving an execution order that guarantees the executing node always has
     data from all its upstream nodes.
 
     Args:
-        *nodes (Node): The nodes whose data topology to analyze
+        nodes (dict[str, Node]): A label-keyed dictionary of nodes from whom to build
+            an execution order based on topological analysis of data flow.
 
     Returns:
         (list[str]): The labels in safe execution order.
@@ -90,7 +102,7 @@ def nodes_to_execution_order(*nodes: Node) -> list[str]:
         # The flattened part is just that we don't care about topological
         # generations that are mutually independent (inefficient but easier for now)
         execution_order = toposort_flatten(
-            nodes_to_data_digraph(*nodes)
+            nodes_to_data_digraph(nodes)
         )
     except CircularDependencyError as e:
         raise ValueError(
@@ -129,7 +141,7 @@ def set_run_connections_according_to_linear_dag(
     try:
         # This is the most primitive sort of topological exploitation we can do
         # It is not efficient if the nodes have executors and can run in parallel
-        execution_order = nodes_to_execution_order(*nodes.values())
+        execution_order = nodes_to_execution_order(nodes)
 
         for i, label in enumerate(execution_order[:-1]):
             next_node = execution_order[i + 1]
