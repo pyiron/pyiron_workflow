cb43cc8df7f5e7976cb75d7bb1c2693176016f8f
liamhuber <liamhuber@greyhavensolutions.com>
2023-10-27 14:08:57 -0700

    Refactor tests

--- tests/unit/test_interfaces.py
+++ tests/unit/test_interfaces.py
@@ -10,21 +10,25 @@ from pyiron_workflow.interfaces import Creator
     sys.version_info[0] == 3 and sys.version_info[1] >= 10, "Only supported for 3.10+"
 )
 class TestCreator(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.creator = Creator()
+        path_to_tests = Path(__file__).parent.parent
+        sys.path.append(str(path_to_tests.resolve()))
+        # Now we can import from `static`
+
     def test_registration(self):
-        creator = Creator()
 
         with self.assertRaises(
             AttributeError,
             msg="Sanity check that the package isn't there yet and the test setup is "
                 "what we want"
         ):
-            creator.demo_nodes
+            self.creator.demo_nodes
 
-        path_to_tests = Path(__file__).parent.parent
-        sys.path.append(str(path_to_tests.resolve()))
-        creator.register("demo", "static.demo_nodes")
+        self.creator.register("demo", "static.demo_nodes")
 
-        node = creator.demo.Add(1, 2)
+        node = self.creator.demo.Add(1, 2)
         self.assertEqual(
             3,
             node(),
