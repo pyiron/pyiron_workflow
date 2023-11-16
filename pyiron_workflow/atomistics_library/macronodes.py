20d4658b090ea3c070d950b2e43039134f34d18c
Jan Janßen <janssen@mpie.de>
2023-11-16 07:43:34 +0100

    Define an internal macro to reduce code duplication

--- pyiron_workflow/atomistics_library/macronodes.py
+++ pyiron_workflow/atomistics_library/macronodes.py
@@ -1,4 +1,5 @@
 from pyiron_workflow.macro import Macro, macro_node
+from pyiron_workflow.function import single_value_node
 from pyiron_workflow.atomistics_library.calculatornodes import calc_with_calculator
 from pyiron_workflow.atomistics_library.tasknodes import (
     get_elastic_matrix_task_generator,
@@ -9,12 +10,28 @@ from pyiron_workflow.atomistics_library.tasknodes import (
 )
 
 
+@single_value_node("instance")
+def get_instance(instance):
+    return instance
+
+
+@macro_node()
+def internal_macro(wf: Macro) -> None:
+    wf.get_instance = get_instance()
+    wf.generate_structures = generate_structures(instance=wf.get_instance)
+    wf.calc_with_calculator = calc_with_calculator(task_dict=wf.generate_structures)
+    wf.fit = analyse_structures(instance=wf.get_instance, output_dict=wf.calc_with_calculator)
+    wf.inputs_map = {
+        "get_instance__instance": "instance",
+        "calc_with_calculator__calculator": "calculator",
+    }
+    wf.outputs_map = {"fit__fit_dict": "fit_dict"}
+
+
 @macro_node()
 def get_energy_volume_curve(wf: Macro) -> None:
     wf.get_task_generator = get_evcurve_task_generator()
-    wf.generate_structures = generate_structures(instance=wf.get_task_generator)
-    wf.calc_with_calculator = calc_with_calculator(task_dict=wf.generate_structures)
-    wf.fit = analyse_structures(instance=wf.get_task_generator, output_dict=wf.calc_with_calculator)
+    wf.internal = internal_macro(instance=wf.get_task_generator)
     wf.inputs_map = {
         "get_task_generator__structure": "structure",
         "get_task_generator__num_points": "num_points",
@@ -23,34 +40,30 @@ def get_energy_volume_curve(wf: Macro) -> None:
         "get_task_generator__vol_range": "vol_range",
         "get_task_generator__axes": "axes",
         "get_task_generator__strains": "strains",
-        "calc_with_calculator__calculator": "calculator",
+        "internal__calculator": "calculator",
     }
-    wf.outputs_map = {"fit__fit_dict": "fit_dict"}
+    wf.outputs_map = {"internal__fit_dict": "fit_dict"}
 
 
 @macro_node()
 def get_elastic_matrix(wf: Macro) -> None:
     wf.get_task_generator = get_elastic_matrix_task_generator()
-    wf.generate_structures = generate_structures(instance=wf.get_task_generator)
-    wf.calc_with_calculator = calc_with_calculator(task_dict=wf.generate_structures)
-    wf.fit = analyse_structures(instance=wf.get_task_generator, output_dict=wf.calc_with_calculator)
+    wf.internal = internal_macro(instance=wf.get_task_generator)
     wf.inputs_map = {
         "get_task_generator__structure": "structure",
         "get_task_generator__num_of_point": "num_of_point",
         "get_task_generator__eps_range": "eps_range",
         "get_task_generator__sqrt_eta": "sqrt_eta",
         "get_task_generator__fit_order": "fit_order",
-        "calc_with_calculator__calculator": "calculator",
+        "internal__calculator": "calculator",
     }
-    wf.outputs_map = {"fit__fit_dict": "fit_dict"}
+    wf.outputs_map = {"internal__fit_dict": "fit_dict"}
 
 
 @macro_node()
 def get_phonons(wf: Macro) -> None:
     wf.get_task_generator = get_phonons_task_generator()
-    wf.generate_structures = generate_structures(instance=wf.get_task_generator)
-    wf.calc_with_calculator = calc_with_calculator(task_dict=wf.generate_structures)
-    wf.fit = analyse_structures(instance=wf.get_task_generator, output_dict=wf.calc_with_calculator)
+    wf.internal = internal_macro(instance=wf.get_task_generator)
     wf.inputs_map = {
         "get_task_generator__structure": "structure",
         "get_task_generator__interaction_range": "interaction_range",
@@ -59,6 +72,6 @@ def get_phonons(wf: Macro) -> None:
         "get_task_generator__dos_mesh": "dos_mesh",
         "get_task_generator__primitive_matrix": "primitive_matrix",
         "get_task_generator__number_of_snapshots": "number_of_snapshots",
-        "calc_with_calculator__calculator": "calculator",
+        "internal__calculator": "calculator",
     }
-    wf.outputs_map = {"fit__fit_dict": "fit_dict"}
+    wf.outputs_map = {"internal__fit_dict": "fit_dict"}
