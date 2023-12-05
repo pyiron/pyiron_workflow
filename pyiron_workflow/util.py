2fd0f6da98ec75ad9669d43572bcd8f5a4e1bcfd
liamhuber <liamhuber@greyhavensolutions.com>
2023-12-05 14:57:40 -0800

    Move DotDict to snippets

--- pyiron_workflow/util.py
+++ pyiron_workflow/util.py
@@ -1,28 +1,6 @@
 from abc import ABCMeta
 
 
-class DotDict(dict):
-    def __getattr__(self, item):
-        return self.__getitem__(item)
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-    def __dir__(self):
-        return set(super().__dir__() + list(self.keys()))
-
-    def __getstate__(self):
-        return self.__dict__
-
-    def __setstate__(self, state):
-        for k, v in state.items():
-            self.__dict__[k] = v
-
-    def to_list(self):
-        """A list of values (order not guaranteed)"""
-        return list(self.values())
-
-
 class SeabornColors:
     """
     Hex codes for the ten `seaborn.color_palette()` colors (plus pure white and black),
--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -6,15 +6,6 @@ import pyiron_workflow.util as util
 
 @skipUnless(version_info[0] == 3 and version_info[1] >= 10, "Only supported for 3.10+")
 class TestUtil(TestCase):
-    def test_dot_dict(self):
-        dd = util.DotDict({'foo': 42})
-
-        self.assertEqual(dd['foo'], dd.foo, msg="Dot access should be equivalent.")
-        dd.bar = "towel"
-        self.assertEqual("towel", dd["bar"], msg="Dot assignment should be equivalent.")
-
-        self.assertListEqual(dd.to_list(), [42, "towel"])
-
     def test_has_post_metaclass(self):
         class Foo(metaclass=util.HasPost):
             def __init__(self, x=0):
