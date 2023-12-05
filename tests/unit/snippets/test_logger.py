7d629ee6d82a01d2f8ee494d6f257ca9a734112a
liamhuber <liamhuber@greyhavensolutions.com>
2023-12-05 13:54:55 -0800

    Move logger

--- tests/unit/snippets/test_logger.py
+++ tests/unit/snippets/test_logger.py
@@ -3,7 +3,7 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 from unittest import TestCase
-from pyiron_base.state.logger import logger
+from pyiron_workflow.snippets.logger import logger
 import os
 import shutil
 
