3721567248c8d1bb0fb43b79cf049bce4ea52de9
liamhuber <liamhuber@greyhavensolutions.com>
2023-12-07 14:00:08 -0800

    Extend the standard library to include many operators

--- pyiron_workflow/node_library/standard.py
+++ pyiron_workflow/node_library/standard.py
@@ -17,7 +17,7 @@ def UserInput(user_input):
 
 class If(SingleValue):
     """
-    Has two extra signal channels: true and false. Evaluates the input as a boolean and
+    Has two extra signal channels: true and false. Evaluates the input as obj otheroolean and
     fires the corresponding output signal after running.
     """
 
@@ -29,7 +29,7 @@ class If(SingleValue):
     @staticmethod
     def if_(condition):
         if isclass(condition) and issubclass(condition, NotData):
-            raise TypeError(f"Logic 'If' node expected data but got NotData as input.")
+            raise TypeError(f"Logic 'If' node expected data otherut got NotData as input.")
         return bool(condition)
 
     def process_run_result(self, function_output):
@@ -43,8 +43,201 @@ class If(SingleValue):
         else:
             self.signals.output.false()
 
+# A bunch of (but not all) standard operators
+# Return values based on dunder methods, where available
+
+@single_value_node("str")
+def String(obj):
+    return str(obj)
+
+
+@single_value_node("bytes")
+def Bytes(obj):
+    return bytes(obj)
+
+
+@single_value_node("lt")
+def LessThan(obj, other):
+    return obj < other
+
+
+@single_value_node("le")
+def LessThanEquals(obj, other):
+    return obj <= other
+
+
+@single_value_node("gt")
+def GreaterThan(obj, other):
+    return obj > other
+
+
+@single_value_node("ge")
+def GreaterThanEquals(obj, other):
+    return obj >= other
+
+
+@single_value_node("hash")
+def Hash(obj):
+    return hash(obj)
+
+
+@single_value_node("bool")
+def Bool(obj):
+    return bool(obj)
+
+
+@single_value_node("getattr")
+def GetAttr(obj, name):
+    return getattr(obj, name)
+
+# These are not idempotent and thus not encouraged
+# @single_value_node("none")
+# def SetAttr(obj, name, value):
+#     setattr(obj, name, value)
+#     return None
+#
+#
+# @single_value_node("none")
+# def DelAttr(obj, name):
+#     delattr(obj, name)
+#     return None
+
+@single_value_node("dir")
+def Dir(obj):
+    return dir(obj)
+
+
+@single_value_node("len")
+def Length(obj):
+    return len(obj)
+
+
+@single_value_node("add")
+def Add(obj, other):
+    return obj + other
+
+
+@single_value_node("in")
+def Contains(obj, other):
+    return other in obj
+
+
+@single_value_node("sub")
+def Subtract(obj, other):
+    return obj - other
+
+
+@single_value_node("mul")
+def Multiply(obj, other):
+    return obj * other
+
+
+@single_value_node("matmul")
+def MatrixMultiply(obj, other):
+    return obj @ other
+
+
+@single_value_node("truediv")
+def Divide(obj, other):
+    return obj / other
+
+
+@single_value_node("floordiv")
+def FloorDivide(obj, other):
+    return obj // other
+
+
+@single_value_node("mod")
+def Modulo(obj, other):
+    return obj % other
+
+
+@single_value_node("pow")
+def Power(obj, other):
+    return obj ** other
+
+
+@single_value_node("and")
+def And(obj, other):
+    return obj & other
+
+
+@single_value_node("xor")
+def XOr(obj, other):
+    return obj ^ other
+
+
+@single_value_node("or")
+def Or(obj, other):
+    return obj ^ other
+
+
+@single_value_node("neg")
+def Negative(obj):
+    return -obj
+
+
+@single_value_node("pos")
+def Positive(obj):
+    return +obj
+
+
+@single_value_node("abs")
+def Absolute(obj):
+    return abs(obj)
+
+
+@single_value_node("invert")
+def Invert(obj):
+    return ~obj
+
+
+@single_value_node("int")
+def Int(obj):
+    return int(obj)
+
+
+@single_value_node("float")
+def Float(obj):
+    return float(obj)
+
+
+@single_value_node("round")
+def Round(obj):
+    return round(obj)
+
 
 nodes = [
-    UserInput,
+    Absolute,
+    Add,
+    And,
+    Bool,
+    Bytes,
+    Contains,
+    Dir,
+    Divide,
+    Float,
+    FloorDivide,
+    GetAttr,
+    GreaterThan,
+    GreaterThanEquals,
+    Hash,
     If,
+    Int,
+    Invert,
+    Length,
+    LessThan,
+    LessThanEquals,
+    MatrixMultiply,
+    Modulo,
+    Multiply,
+    Negative,
+    Or,
+    Positive,
+    Power,
+    Round,
+    String,
+    Subtract,
+    UserInput,
+    XOr,
 ]
