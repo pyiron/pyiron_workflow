01a77832b4478a9218ef8b016a9e2426b1dc6b6d
liamhuber <liamhuber@greyhavensolutions.com>
2023-12-13 12:09:00 -0800

    Make sure nodes belong to their macro parent
    `create` got changed so it doesn't implicitly parent nodes when being called from an instance, so we need to manually assign the nodes now.

--- pyiron_workflow/node_library/atomistic_codes.py
+++ pyiron_workflow/node_library/atomistic_codes.py
@@ -7,31 +7,31 @@ from pyiron_workflow.node_library.dev_tools import set_replacer
 def Lammps(wf: Macro) -> None:
     from pyiron_contrib.tinybase.shell import ExecutablePathResolver
 
-    structure = wf.create.lammps.Structure()
+    wf.structure = wf.create.lammps.Structure()
 
-    potential = wf.create.lammps.Potential(structure=structure)
+    wf.potential = wf.create.lammps.Potential(structure=wf.structure)
 
-    wf.list_pots = wf.create.lammps.ListPotentials(structure=structure)
+    wf.list_pots = wf.create.lammps.ListPotentials(structure=wf.structure)
 
     wf.calc = wf.create.lammps.CalcStatic()
     wf.calc_select = set_replacer(wf.calc, get_calculators())
 
-    init_lammps = wf.create.lammps.InitLammps(
+    wf.init_lammps = wf.create.lammps.InitLammps(
         structure=wf.structure,
-        potential=potential,
+        potential=wf.potential,
         calculator=wf.calc.outputs.calculator,
         # working_directory="test2",
     )
-    init_lammps.inputs.working_directory = init_lammps.working_directory.path.__str__()
-    shell = wf.create.lammps.Shell(
+    wf.init_lammps.inputs.working_directory = wf.init_lammps.working_directory.path.__str__()
+    wf.shell = wf.create.lammps.Shell(
         command=ExecutablePathResolver(module="lammps", code="lammps").path(),
-        working_directory=init_lammps.outputs.path,
+        working_directory=wf.init_lammps.outputs.path,
     )
 
-    parser_log_file = wf.create.lammps.ParserLogFile(log_file=shell.outputs.log)
-    parser_dump_file = wf.create.lammps.ParserDumpFile(dump_file=shell.outputs.dump)
-    collect = wf.create.lammps.Collect(
-        out_dump=parser_dump_file.outputs.dump, out_log=parser_log_file.outputs.log
+    wf.parser_log_file = wf.create.lammps.ParserLogFile(log_file=wf.shell.outputs.log)
+    wf.parser_dump_file = wf.create.lammps.ParserDumpFile(dump_file=wf.shell.outputs.dump)
+    wf.collect = wf.create.lammps.Collect(
+        out_dump=wf.parser_dump_file.outputs.dump, out_log=wf.parser_log_file.outputs.log
     )
 
     wf.inputs_map = {
