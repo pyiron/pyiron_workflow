bf3f50638a1726c5c299233e48f8b506ec65b9cd
liamhuber <liamhuber@greyhavensolutions.com>
2024-01-24 11:21:38 -0800

    :bug: Fix typo in __getstate__ return
    Actually use the state we create!

--- pyiron_workflow/node.py
+++ pyiron_workflow/node.py
@@ -1073,7 +1073,7 @@ class Node(HasToDict, ABC, metaclass=AbstractHasPost):
         # _but_ if the user is just passing instructions on how to _build_ an executor,
         # we'll trust that those serialize OK (this way we can, hopefully, eventually
         # support nesting executors!)
-        return self.__dict__
+        return state
 
     def __setstate__(self, state):
         # Update instead of overriding in case some other attributes were added on the
