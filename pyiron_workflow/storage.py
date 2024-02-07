88a0f8a11894d81d0704750990e6ffb0b85ce66f
liamhuber <liamhuber@greyhavensolutions.com>
2024-02-07 12:40:47 -0800

    Add the report to the error message

--- pyiron_workflow/storage.py
+++ pyiron_workflow/storage.py
@@ -53,7 +53,9 @@ class StorageInterface:
                     f"{self.node.label} cannot be saved with h5io because it (or one "
                     f"of its child nodes) has a type that cannot be imported. Did you "
                     f"dynamically define this node? Try using the node wrapper as a "
-                    f"decorator instead."
+                    f"decorator instead. \n"
+                    f"Import readiness report: \n"
+                    f"{self.node.import_readiness_report()}"
                 )
             h5io.write_hdf5(
                 fname=self._h5io_storage_file_path,
