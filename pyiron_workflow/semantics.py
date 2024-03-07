be1e869d3eadb1b3895e3b373f94cc5114e05883
liamhuber <liamhuber@greyhavensolutions.com>
2024-03-07 12:16:06 -0800

    Break responsibility for children into another class

--- pyiron_workflow/semantics.py
+++ pyiron_workflow/semantics.py
@@ -34,10 +34,8 @@ class Semantic(ABC):
     ):
         self._label = None
         self._parent = None
-        self._children = bidict()
         self.label = label
         self.parent = parent
-        self.strict_naming = strict_naming
         super().__init__(self, *args, **kwargs)
 
     @property
@@ -57,13 +55,13 @@ class Semantic(ABC):
         return self._parent
 
     @parent.setter
-    def parent(self, new_parent: Semantic | None) -> None:
+    def parent(self, new_parent: HasSemanticChildren | None) -> None:
         if new_parent is self._parent:
             # Exit early if nothing is changing
             return
 
         if new_parent is not None:
-            if not isinstance(new_parent, Semantic):
+            if not isinstance(new_parent, HasSemanticChildren):
                 raise ValueError(
                     f"Expected None or another {Semantic.__name__} for the "
                     f"semantic parent of {self.label}, but got {new_parent}"
@@ -73,6 +71,66 @@ class Semantic(ABC):
             self._parent.remove_child(self)
         self._parent = new_parent
         self._parent.add_child(self)
+
+    @property
+    def path(self) -> str:
+        """
+        The path of node labels from the graph root (parent-most node) down to this
+        node.
+        """
+        prefix = "" if self.parent is None else self.parent.path
+        return prefix + self.delimiter + self.label
+
+    @property
+    def root(self) -> Semantic:
+        """The parent-most object in this semantic path; may be self."""
+        return self if self.parent is None else self.root
+
+    def __getstate__(self):
+        state = dict(self.__dict__)
+        state["_parent"] = None
+        # Comment on moving this to semantics)
+        # Basically we want to avoid recursion during (de)serialization; when the
+        # parent object is deserializing itself, _it_ should know who its children are
+        # and inform them of this.
+        #
+        # Original comment when this behaviour belonged to node)
+        # I am not at all confident that removing the parent here is the _right_
+        # solution.
+        # In order to run composites on a parallel process, we ship off just the nodes
+        # and starting nodes.
+        # When the parallel process returns these, they're obviously different
+        # instances, so we re-parent them back to the receiving composite.
+        # At the same time, we want to make sure that the _old_ children get orphaned.
+        # Of course, we could do that directly in the composite method, but it also
+        # works to do it here.
+        # Something I like about this, is it also means that when we ship groups of
+        # nodes off to another process with cloudpickle, they're definitely not lugging
+        # along their parent, its connections, etc. with them!
+        # This is all working nicely as demonstrated over in the macro test suite.
+        # However, I have a bit of concern that when we start thinking about
+        # serialization for storage instead of serialization to another process, this
+        # might introduce a hard-to-track-down bug.
+        # For now, it works and I'm going to be super pragmatic and go for it, but
+        # for the record I am admitting that the current shallowness of my understanding
+        # may cause me/us headaches in the future.
+        # -Liam
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(**state)
+
+
+class HasSemanticChildren(ABC):
+    def __init__(
+        self,
+        *args,
+        strict_naming: bool = True,
+        **kwargs
+    ):
+        self._children = bidict()
+        self.strict_naming = strict_naming
+        super().__init__(self, *args, **kwargs)
 
     @property
     def children(self) -> bidict[str: Semantic]:
@@ -199,52 +257,3 @@ class Semantic(ABC):
         child.parent = None
 
         return child
-
-
-    @property
-    def path(self) -> str:
-        """
-        The path of node labels from the graph root (parent-most node) down to this
-        node.
-        """
-        prefix = "" if self.parent is None else self.parent.path
-        return prefix + self.delimiter + self.label
-
-    @property
-    def root(self) -> Semantic:
-        """The parent-most object in this semantic path; may be self."""
-        return self if self.parent is None else self.root
-
-    def __getstate__(self):
-        state = dict(self.__dict__)
-        state["_parent"] = None
-        # Comment on moving this to semantics)
-        # Basically we want to avoid recursion during (de)serialization; when the
-        # parent object is deserializing itself, _it_ should know who its children are
-        # and inform them of this.
-        #
-        # Original comment when this behaviour belonged to node)
-        # I am not at all confident that removing the parent here is the _right_
-        # solution.
-        # In order to run composites on a parallel process, we ship off just the nodes
-        # and starting nodes.
-        # When the parallel process returns these, they're obviously different
-        # instances, so we re-parent them back to the receiving composite.
-        # At the same time, we want to make sure that the _old_ children get orphaned.
-        # Of course, we could do that directly in the composite method, but it also
-        # works to do it here.
-        # Something I like about this, is it also means that when we ship groups of
-        # nodes off to another process with cloudpickle, they're definitely not lugging
-        # along their parent, its connections, etc. with them!
-        # This is all working nicely as demonstrated over in the macro test suite.
-        # However, I have a bit of concern that when we start thinking about
-        # serialization for storage instead of serialization to another process, this
-        # might introduce a hard-to-track-down bug.
-        # For now, it works and I'm going to be super pragmatic and go for it, but
-        # for the record I am admitting that the current shallowness of my understanding
-        # may cause me/us headaches in the future.
-        # -Liam
-        return state
-
-    def __setstate__(self, state):
-        self.__dict__.update(**state)
