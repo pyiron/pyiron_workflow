346e939a451a8c19f24ea5c2e81798008222002b
liamhuber <liamhuber@greyhavensolutions.com>
2024-04-27 10:18:44 -0700

    Revert singleton

--- pyiron_workflow/snippets/singleton.py
+++ pyiron_workflow/snippets/singleton.py
@@ -3,13 +3,21 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 """
-Tools for singleton objects.
+Utility functions used in pyiron.
+In order to be accessible from anywhere in pyiron, they *must* remain free of any imports from pyiron!
 """
-
-from __future__ import annotations
-
 from abc import ABCMeta
-from functools import wraps
+
+__author__ = "Joerg Neugebauer, Jan Janssen"
+__copyright__ = (
+    "Copyright 2020, Max-Planck-Institut für Eisenforschung GmbH - "
+    "Computational Materials Design (CM) Department"
+)
+__version__ = "1.0"
+__maintainer__ = "Jan Janssen"
+__email__ = "janssen@mpie.de"
+__status__ = "production"
+__date__ = "Sep 1, 2017"
 
 
 class Singleton(ABCMeta):
@@ -26,103 +34,3 @@ class Singleton(ABCMeta):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
-
-
-class _RegisteredFactory(metaclass=Singleton):
-    """
-    For making dynamically created classes the same class.
-
-    Used under-the-hood in the public facing decorator below.
-    """
-
-    def __init_subclass__(cls, /, factory_function: callable[..., type]):
-        cls.factory_function = staticmethod(factory_function)
-        cls.registry = {}
-
-    def __call__(self, *args, **kwargs):
-        constructed = self.factory_function(*args, **kwargs)
-        try:
-            return self.registry[constructed.__name__]
-        except KeyError:
-            self.registry[constructed.__name__] = constructed
-            return constructed
-
-    def __reduce__(self):
-        return (
-            _FactoryTown().get_registered_factory,
-            (self.factory_function,),
-        )
-
-
-class _FactoryTown(metaclass=Singleton):
-    """
-    A singleton to hold existing factories, so that if we wrap the same factory
-    function more than once, we always get back the same factory.
-    """
-
-    factories = {}
-
-    def get_registered_factory(
-        self, factory_function: callable[..., type]
-    ) -> type[_RegisteredFactory]:
-        try:
-            return self.factories[id(factory_function)]
-        except KeyError:
-            factory_class = type(
-                f"{_RegisteredFactory.__name__}{factory_function.__name__.title()}",
-                (_RegisteredFactory,),
-                {},
-                factory_function=factory_function,
-            )
-            factory = wraps(factory_function)(factory_class())
-            self.factories[id(factory_function)] = factory
-            return factory
-
-
-def registered_factory(factory_function: callable[..., type]):
-    """
-    A decorator for wrapping class factories.
-
-    Wrapped factories return the _same object_ when they would generate a class with
-    the same name as they have generated before. I.e. a sort of singleton-class
-    generator where the classes themselves are singleton, not their instances.
-
-    Args:
-        factory_function (callabe[..., type]): The class factory to wrap.
-
-    Returns:
-        (_RegisteredFactory): A factory instance that will return the same class object
-            whenever the factory method would return a class whose name has been seen
-            before.
-
-    Example:
-        >>> from abc import ABC
-        >>>
-        >>> from pyiron_workflow.snippets.singleton import registered_factory
-        >>>
-        >>> class Foo(ABC):
-        ...     def __init_subclass__(cls, /, n=0, **kwargs):
-        ...         super().__init_subclass__(**kwargs)
-        ...         cls.n = n
-        >>>
-        >>> @registered_factory
-        ... def foo_factory(n):
-        ...     return type(
-        ...         f"{Foo.__name__}{n}",
-        ...         (Foo,),
-        ...         {},
-        ...         n=n
-        ...     )
-        >>>
-        >>> FooTwo = foo_factory(2)
-        >>> Foo2 = foo_factory(2)
-        >>> print(FooTwo.__name__, FooTwo.n)
-        Foo2 2
-
-        >>> print(Foo2.__name__, Foo2.n)
-        Foo2 2
-
-        >>> print(FooTwo is Foo2)
-        True
-    """
-    return _FactoryTown().get_registered_factory(factory_function)
--- tests/unit/snippets/test_singleton.py
+++ tests/unit/snippets/test_singleton.py
@@ -1,7 +1,5 @@
-from abc import ABC
 import unittest
-
-from pyiron_workflow.snippets.singleton import Singleton, registered_factory
+from pyiron_workflow.snippets.singleton import Singleton
 
 
 class TestSingleton(unittest.TestCase):
@@ -15,102 +13,6 @@ class TestSingleton(unittest.TestCase):
         self.assertIs(f1, f2)
         f2.x = 2
         self.assertEqual(2, f1.x)
-
-
-class TestRegisteredFactory(unittest.TestCase):
-    def test_decorator(self):
-        class Foo(ABC):
-            def __init_subclass__(cls, /, n=0, **kwargs):
-                super().__init_subclass__(**kwargs)
-                cls.n = n
-
-        def foo_factory(n):
-            """The foo factory docstring."""
-            return type(
-                f"{Foo.__name__}{n}",
-                (Foo,),
-                {},
-                n=n
-            )
-
-        FooTwo = foo_factory(2)
-        Foo2 = foo_factory(2)
-        self.assertEqual(
-            FooTwo.__name__,
-            Foo2.__name__,
-            msg="Sanity check"
-        )
-        self.assertIsNot(
-            FooTwo,
-            Foo2,
-            msg="Sanity check"
-        )
-        self.assertEqual(
-            2,
-            Foo2.n,
-            msg="Sanity check"
-        )
-
-        Foo3 = foo_factory(3)
-        self.assertIsNot(
-            Foo3,
-            Foo2,
-            msg="Sanity check"
-        )
-
-        registered_foo_factory = registered_factory(foo_factory)
-        FooTwo = registered_foo_factory(2)
-        Foo2 = registered_foo_factory(2)
-        self.assertEqual(
-            FooTwo.__name__,
-            Foo2.__name__,
-            msg="Sanity check"
-        )
-        self.assertIs(
-            FooTwo,
-            Foo2,
-            msg="The point of the registration is that dynamically generated classes "
-                "with the same name from the same generator should wind up being the "
-                "same class"
-        )
-        self.assertEqual(
-            "The foo factory docstring.",
-            registered_foo_factory.__doc__,
-            msg="The wrapper should preserve the factory's docstring"
-        )
-
-        re_registered_foo_factory = registered_factory(foo_factory)
-        self.assertIs(
-            registered_foo_factory,
-            re_registered_foo_factory,
-            msg="The factories themselves are singletons based on the id of the "
-                "factory function they use; If you register the same factory function "
-                "twice, you should get the same factory back."
-        )
-        self.assertIs(
-            re_registered_foo_factory(2),
-            Foo2,
-            msg="From the above, it should hold trivially that building the same class "
-                "from each factory gives the same class."
-        )
-
-        @registered_factory
-        def different_but_similar(n):
-            """The foo factory docstring."""
-            return foo_factory(n)
-
-        Foo2b = different_but_similar(2)
-        self.assertEqual(
-            Foo2b.__name__,
-            Foo2.__name__,
-            msg="Sanity check. It's the same factory behaviour after all."
-        )
-        self.assertIsNot(
-            Foo2b,
-            Foo2,
-            msg="These come from two separate registered factories, each of which is "
-                "maintaining its own internal registration list."
-        )
 
 
 if __name__ == '__main__':
