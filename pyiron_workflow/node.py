bcc8be40c6b5174a9837f0ce8079877ea3099984
liamhuber <liamhuber@greyhavensolutions.com>
2024-08-09 10:07:03 -0700

    :bug: Import storage mixin
    I must have screwed up the import line when doing the rebase merge on GitHub

--- pyiron_workflow/node.py
+++ pyiron_workflow/node.py
@@ -21,6 +21,7 @@ from pyiron_workflow.mixin.injection import HasIOWithInjection
 from pyiron_workflow.mixin.run import Runnable, ReadinessError
 from pyiron_workflow.mixin.semantics import Semantic
 from pyiron_workflow.mixin.single_output import ExploitsSingleOutput
+from pyiron_workflow.mixin.storage import HasPickleStorage
 from pyiron_workflow.topology import (
     get_nodes_in_data_tree,
     set_run_connections_according_to_linear_dag,
