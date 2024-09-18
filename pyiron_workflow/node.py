1dcae11c7e8818706393274cf31e0d290260b18c
liamhuber <liamhuber@greyhavensolutions.com>
2024-09-18 14:04:07 -0700

    Remove unused import

--- pyiron_workflow/node.py
+++ pyiron_workflow/node.py
@@ -10,7 +10,6 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from concurrent.futures import Future
 from importlib import import_module
-from inspect import getsource
 from typing import Any, Literal, Optional, TYPE_CHECKING
 
 import cloudpickle
