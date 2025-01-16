08ff9b292a9af129029f85223d19171c94538456
liamhuber <liamhuber@greyhavensolutions.com>
2025-01-16 13:42:46 -0800

    Provide a type-compliant default
    It never actually matters with the current logic, because of all the checks if parent is None and the fact that it is otherwise hinted to be at least a `Composite`, but it shuts mypy up and it does zero harm.
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_workflow/node.py
+++ pyiron_workflow/node.py
@@ -631,7 +631,7 @@ class Node(
 
         try:
             parent_starting_nodes = (
-                self.parent.starting_nodes if self.parent is not None else None
+                self.parent.starting_nodes if self.parent is not None else []
             )  # We need these for state recovery later, even if we crash
 
             if len(data_tree_starters) == 1 and data_tree_starters[0] is self:
