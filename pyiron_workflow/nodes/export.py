e08708ae711eb7c1ecb07d828c8cd0e3aba6e65c
samwaseda <o.waseda@mpie.de>
2025-02-11 12:48:22 +0000

    replace public by private functions

--- pyiron_workflow/nodes/export.py
+++ pyiron_workflow/nodes/export.py
@@ -3,7 +3,7 @@ from pyiron_workflow import NOT_DATA
 from pyiron_workflow.channels import Channel
 
 
-def extract_data(item: Channel) -> dict:
+def _extract_data(item: Channel) -> dict:
     data = {}
     for key in ["default", "value"]:
         if getattr(item, key) is not NOT_DATA:
@@ -13,13 +13,13 @@ def extract_data(item: Channel) -> dict:
     return data
 
 
-def is_internal_connection(channel: Channel, workflow: Composite, io_: str) -> bool:
+def _is_internal_connection(channel: Channel, workflow: Composite, io_: str) -> bool:
     if not channel.connected:
         return False
     return any([channel.connections[0] in getattr(n, io_) for n in workflow])
 
 
-def get_scoped_label(channel: Channel, io_: str) -> str:
+def _get_scoped_label(channel: Channel, io_: str) -> str:
     return channel.scoped_label.replace("__", f".{io_}.")
 
 
@@ -33,33 +33,33 @@ def get_universal_dict(workflow: Composite, with_values: bool = True) -> dict:
                 data["edges"].append(
                     (
                         f"inputs.{inp.scoped_label}",
-                        get_scoped_label(inp.value_receiver, "inputs"),
+                        _get_scoped_label(inp.value_receiver, "inputs"),
                     )
                 )
         for node in workflow:
             label = node.label
             data["nodes"][label] = get_universal_dict(node, with_values=with_values)
             for out in node.outputs:
-                if is_internal_connection(out, workflow, "inputs"):
+                if _is_internal_connection(out, workflow, "inputs"):
                     data["edges"].append(
                         (
-                            get_scoped_label(out, "outputs"),
-                            get_scoped_label(out.connections[0], "inputs"),
+                            _get_scoped_label(out, "outputs"),
+                            _get_scoped_label(out.connections[0], "inputs"),
                         )
                     )
                 elif out.value_receiver is not None:
                     data["edges"].append(
                         (
-                            get_scoped_label(out, "outputs"),
+                            _get_scoped_label(out, "outputs"),
                             f"outputs.{out.value_receiver.scoped_label}",
                         )
                     )
         for io_ in ["inputs", "outputs"]:
             for inp in getattr(workflow, io_):
-                data[io_][inp.scoped_label] = extract_data(inp)
+                data[io_][inp.scoped_label] = _extract_data(inp)
     else:
         for io_ in ["inputs", "outputs"]:
             for inp in getattr(workflow, io_):
-                data[io_][inp.label] = extract_data(inp)
+                data[io_][inp.label] = _extract_data(inp)
         data["function"] = workflow.node_function
     return data
