d11a3442aae5683476152c898f2a579ebb778280
samwaseda <o.waseda@mpie.de>
2025-05-14 13:52:20 +0000

    Correct imports

--- pyiron_ontology/parser.py
+++ pyiron_ontology/parser.py
@@ -1,7 +1,8 @@
 from semantikon.converter import parse_input_args, parse_output_args
-from semantikon.ontology import get_knowledge_graph, PNS
+from semantikon.ontology import get_knowledge_graph, SNS
 from rdflib import Graph
-from pyiron_workflow import NOT_DATA, Workflow, Macro
+from pyiron_workflow import Workflow, Macro
+from pyiron_workflow.api import NOT_DATA
 from pyiron_workflow.node import Node
 from pyiron_workflow.nodes.composite import Composite
 from pyiron_workflow.channels import Channel
