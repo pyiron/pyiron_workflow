8a191cff33a8b11f26379b574826da6ac37bf921
liamhuber <liamhuber@greyhavensolutions.com>
2025-08-18 12:30:50 -0700

    Add a node-class suggester
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_workflow/suggest.py
+++ pyiron_workflow/suggest.py
@@ -2,6 +2,7 @@ import bidict
 from semantikon import metadata as meta
 
 from pyiron_workflow import channels, io, type_hinting
+from pyiron_workflow.channels import ChannelConnectionError
 from pyiron_workflow.nodes import static_io
 
 
@@ -53,3 +54,50 @@ def suggest_connections(channel: channels.DataChannel):
                 candidates.append((sibling, candidate))
 
     return candidates
+
+
+def suggest_nodes(
+    channel: channels.DataChannel, *corpus: type[static_io.StaticNode]
+) -> list[type[static_io.StaticNode]]:
+    target_hint, proximate_graph, suggest_for_input = _parse_levers(channel)
+
+    candidate_classes = []
+    for node_class in corpus:
+        preview = (
+            node_class.preview_outputs()
+            if suggest_for_input
+            else node_class.preview_inputs()
+        )
+
+        for label, preview_value in preview.items():
+            candidate_hint = preview_value if suggest_for_input else preview_value[0]
+
+            if candidate_hint is None:
+                continue
+
+            upstream_hint, downstream_hint = (
+                (candidate_hint, target_hint)
+                if suggest_for_input
+                else (target_hint, candidate_hint)
+            )
+
+            if not type_hinting.type_hint_is_as_or_more_specific_than(
+                upstream_hint, downstream_hint
+            ):
+                continue
+
+            trial_label = f"__ontological_candidate_{node_class.__name__}"
+            trial_child = proximate_graph.add_child(node_class(label=trial_label))
+            try:
+                channel.connect(
+                    trial_child.outputs[label]
+                    if suggest_for_input
+                    else trial_child.inputs[label]
+                )
+                candidate_classes.append(node_class)
+            except ChannelConnectionError:
+                continue
+            finally:
+                proximate_graph.remove_child(trial_label)
+
+    return candidate_classes
