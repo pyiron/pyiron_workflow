770f76c2fb85dc04db2ad595766b4d098ac6bfce
liamhuber <liamhuber@greyhavensolutions.com>
2025-08-20 12:37:12 -0700

    Revert is_composite
    Since `semantikon.ontology` imports slowly, we break the circular import inside the `channels` module anyhow
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_workflow/knowledge.py
+++ pyiron_workflow/knowledge.py
@@ -1,23 +1,17 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, TypeGuard
+from typing import TYPE_CHECKING, Any
 
 import rdflib
 from semantikon import ontology as onto
 
 from pyiron_workflow.data import NOT_DATA
+from pyiron_workflow.nodes.composite import Composite
 
 if TYPE_CHECKING:
     from pyiron_workflow.channels import Channel
     from pyiron_workflow.node import Node
-    from pyiron_workflow.nodes.composite import Composite
     from pyiron_workflow.workflow import Workflow
-
-
-def is_composite(node: Node) -> TypeGuard[Composite]:
-    from pyiron_workflow.nodes.composite import Composite  # noqa: PLC0415
-
-    return isinstance(node, Composite)
 
 
 def _extract_data(item: Channel, with_values=True, with_default=True) -> dict:
@@ -60,7 +54,7 @@ def _io_to_dict(
     data: dict[str, dict] = {"inputs": {}, "outputs": {}}
     for io_ in ["inputs", "outputs"]:
         for inp in getattr(node, io_):
-            if is_composite(node):
+            if isinstance(node, Composite):
                 data[io_][inp.scoped_label] = _extract_data(
                     inp, with_values=with_values, with_default=with_default
                 )
@@ -121,7 +115,7 @@ def _export_composite_to_dict(
             )
     for node in workflow:
         label = node.label
-        if is_composite(node):
+        if isinstance(node, Composite):
             data["nodes"][label] = _export_composite_to_dict(
                 node, with_values=with_values
             )
@@ -152,7 +146,7 @@ def _export_composite_to_dict(
 def export_to_dict(
     workflow: Node, with_values: bool = True, with_default: bool = True
 ) -> dict:
-    if is_composite(workflow):
+    if isinstance(workflow, Composite):
         return _export_composite_to_dict(workflow, with_values=with_values)
     return _export_node_to_dict(
         workflow, with_values=with_values, with_default=with_default
