b18ac62314e7392d07ba26286b418c1495c025db
liamhuber <liamhuber@greyhavensolutions.com>
2026-05-05 08:18:13 -0700

    Remove dependence on private semantikon method
    It is so short and simple, I just duplicate it here.
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_workflow/_wfms/helpers.py
+++ pyiron_workflow/_wfms/helpers.py
@@ -1,11 +1,20 @@
-from semantikon import converter, flowrep_dict
+from typing import Annotated, Any, get_args, get_origin
+
+from semantikon import converter
 from semantikon import datastructure as sds
 
 
 def annotation_to_type_hint(annotation) -> type | None:
-    return (
-        flowrep_dict._unwrap_annotated(annotation) if annotation is not None else None
-    )
+    return _unwrap_annotated(annotation) if annotation is not None else None
+
+
+def _unwrap_annotated(annotation: Any) -> Any:
+    """
+    Strip `Annotated` wrappers, returning just the base type hint.
+    """
+    if get_origin(annotation) is Annotated:
+        return get_args(annotation)[0]
+    return annotation
 
 
 def annotation_to_type_metadata(annotation) -> sds.TypeMetadata | None:
