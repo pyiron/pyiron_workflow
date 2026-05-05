b46d883c2f1870db9c735936e0ab54d5c12ac2cc
liamhuber <liamhuber@greyhavensolutions.com>
2026-05-05 13:40:04 -0700

    Draft prospective for-data
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_workflow/_wfms/flowcontrol.py
+++ pyiron_workflow/_wfms/flowcontrol.py
@@ -2,28 +2,42 @@ from __future__ import annotations
 
 from flowrep.api import schemas as frs
 
-from pyiron_workflow._wfms import execution
-from pyiron_workflow._wfms.datatypes import FlowControl, Node, NodeMap
+from pyiron_workflow._wfms import dag, execution
+from pyiron_workflow._wfms.datatypes import FlowControl, Graph, NodeMap
 
 
 class ForEach(FlowControl):
-    body_node: Node
+
+    def __init__(
+        self,
+        label: frs.Label,
+        recipe: frs.WorkflowNode,
+        *,
+        owner: Graph | None = None,
+        history_limit: int = 10,
+    ):
+        super().__init__(label, recipe, owner=owner, history_limit=history_limit)
+
+        bn = self.recipe.body_node
+        self._prospective_nodes = NodeMap(
+            self, dag.recipe2static(bn.label, bn.node, owner=self)
+        )
 
     @property
     def prospective_input_edges(self) -> frs.InputEdges:
-        raise NotImplementedError()
+        return self.recipe.input_edges
 
     @property
     def prospective_edges(self) -> frs.Edges:
-        raise NotImplementedError()
+        return {}
 
     @property
     def prospective_output_edges(self) -> frs.OutputEdges:
-        raise NotImplementedError()
+        return self.recipe.output_edges
 
     @property
     def prospective_nodes(self) -> NodeMap:
-        raise NotImplementedError()
+        return self._prospective_nodes
 
     def evaluate(
         self, run: execution.Run[frs.LiveWorkflow], config: execution.RunConfig
@@ -48,7 +62,12 @@ class ForEach(FlowControl):
     def _build_retrospective_nodes(
         self, run: execution.Run[frs.LiveWorkflow]
     ) -> NodeMap:
-        raise NotImplementedError()
+        node_map = NodeMap(self)
+        for step in run.steps:
+            node = dag.recipe2static(step.label, step.run.result.recipe, owner=self)
+            node.current_run = step.run
+            node_map[step.label] = node
+        return node_map
 
 
 class If(FlowControl):
