47e4f5090bbf6e1ce77588862692a28db6176332
liamhuber <liamhuber@greyhavensolutions.com>
2026-05-05 08:11:10 -0700

    Frame flow controls
    To get mypy to shut up about them not fulfilling their promises.
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_workflow/_wfms/flowcontrol.py
+++ pyiron_workflow/_wfms/flowcontrol.py
@@ -1,16 +1,140 @@
 from __future__ import annotations
 
-from pyiron_workflow._wfms.datatypes import FlowControl, Node
+from _wfms import execution
+from flowrep.api import schemas as frs
+
+from pyiron_workflow._wfms.datatypes import FlowControl, Node, NodeMap
 
 
 class ForEach(FlowControl):  # Not implemented
     body_node: Node
 
+    @property
+    def input_edges(self) -> frs.InputEdges:
+        raise NotImplementedError()
 
-class If(FlowControl): ...  # Not implemented
+    @property
+    def edges(self) -> frs.Edges:
+        raise NotImplementedError()
+
+    @property
+    def output_edges(self) -> frs.OutputEdges:
+        raise NotImplementedError()
+
+    @property
+    def nodes(self) -> NodeMap:
+        raise NotImplementedError()
+
+    @property
+    def prospective_input_edges(self) -> frs.InputEdges:
+        raise NotImplementedError()
+
+    @property
+    def prospective_edges(self) -> frs.Edges:
+        raise NotImplementedError()
+
+    @property
+    def prospective_output_edges(self) -> frs.OutputEdges:
+        raise NotImplementedError()
+
+    def evaluate(self, run: execution.Run[frs.LiveWorkflow]) -> None:
+        raise NotImplementedError()
 
 
-class Try(FlowControl): ...  # Not implemented
+class If(FlowControl):
+    @property
+    def input_edges(self) -> frs.InputEdges:
+        raise NotImplementedError()
+
+    @property
+    def edges(self) -> frs.Edges:
+        raise NotImplementedError()
+
+    @property
+    def output_edges(self) -> frs.OutputEdges:
+        raise NotImplementedError()
+
+    @property
+    def nodes(self) -> NodeMap:
+        raise NotImplementedError()
+
+    @property
+    def prospective_input_edges(self) -> frs.InputEdges:
+        raise NotImplementedError()
+
+    @property
+    def prospective_edges(self) -> frs.Edges:
+        raise NotImplementedError()
+
+    @property
+    def prospective_output_edges(self) -> frs.OutputEdges:
+        raise NotImplementedError()
+
+    def evaluate(self, run: execution.Run[frs.LiveWorkflow]) -> None:
+        raise NotImplementedError()
 
 
-class While(FlowControl): ...  # Not implemented
+class Try(FlowControl):
+    @property
+    def input_edges(self) -> frs.InputEdges:
+        raise NotImplementedError()
+
+    @property
+    def edges(self) -> frs.Edges:
+        raise NotImplementedError()
+
+    @property
+    def output_edges(self) -> frs.OutputEdges:
+        raise NotImplementedError()
+
+    @property
+    def nodes(self) -> NodeMap:
+        raise NotImplementedError()
+
+    @property
+    def prospective_input_edges(self) -> frs.InputEdges:
+        raise NotImplementedError()
+
+    @property
+    def prospective_edges(self) -> frs.Edges:
+        raise NotImplementedError()
+
+    @property
+    def prospective_output_edges(self) -> frs.OutputEdges:
+        raise NotImplementedError()
+
+    def evaluate(self, run: execution.Run[frs.LiveWorkflow]) -> None:
+        raise NotImplementedError()
+
+
+class While(FlowControl):
+    @property
+    def input_edges(self) -> frs.InputEdges:
+        raise NotImplementedError()
+
+    @property
+    def edges(self) -> frs.Edges:
+        raise NotImplementedError()
+
+    @property
+    def output_edges(self) -> frs.OutputEdges:
+        raise NotImplementedError()
+
+    @property
+    def nodes(self) -> NodeMap:
+        raise NotImplementedError()
+
+    @property
+    def prospective_input_edges(self) -> frs.InputEdges:
+        raise NotImplementedError()
+
+    @property
+    def prospective_edges(self) -> frs.Edges:
+        raise NotImplementedError()
+
+    @property
+    def prospective_output_edges(self) -> frs.OutputEdges:
+        raise NotImplementedError()
+
+    def evaluate(self, run: execution.Run[frs.LiveWorkflow]) -> None:
+        raise NotImplementedError()
