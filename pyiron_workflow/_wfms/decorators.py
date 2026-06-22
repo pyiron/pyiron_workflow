1699aea9a2e77f91a0853fd11f9aaa558de81d5d
liamhuber <liamhuber@greyhavensolutions.com>
2026-06-22 13:12:40 -0700

    Make attribute private
    and rename it
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_workflow/_wfms/decorators.py
+++ pyiron_workflow/_wfms/decorators.py
@@ -23,14 +23,14 @@ class _DecoratedFunction(Generic[_RecipeType]):
 
     def __init__(self, wrapped: types.FunctionType):
         self._disallow_locals(wrapped)
-        self.function = wrapped
+        self._decorated_function = wrapped
 
     @property
     def recipe(self) -> _RecipeType:
-        return self.function.flowrep_recipe  # type: ignore[attr-defined]
+        return self._decorated_function.flowrep_recipe  # type: ignore[attr-defined]
 
     def node(self, label: fr.schemas.Label | None = None, /, *positional, **keyword):
-        used_label = self.function.__name__ if label is None else label
+        used_label = self._decorated_function.__name__ if label is None else label
         return self.node_type(used_label, self.recipe, *positional, **keyword)
 
     def run(self, config: execution.RunConfig | None = None, **input_data):
