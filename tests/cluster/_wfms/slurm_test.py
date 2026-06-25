50953d0c8ac02f366de4ebe07d45e36b74e5848c
liamhuber <liamhuber@greyhavensolutions.com>
2026-06-25 10:45:07 -0700

    Add a new slurm test
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

---
+++ tests/cluster/_wfms/slurm_test.py
@@ -0,0 +1,154 @@
+import argparse
+import os
+import pathlib
+import subprocess
+import tempfile
+import time
+
+from pip._internal.utils import datetime
+
+import pyiron_workflow._wfms.api as pwf
+
+
+@pwf.atomic
+def identity(x):
+    return x
+
+
+@pwf.atomic
+def sleepy(t):
+    time.sleep(t)
+    return t
+
+
+@pwf.workflow
+def three_step(t):
+    n1 = identity(t)
+    n2 = sleepy(n1)
+    n3 = identity(n2)
+    return n3
+
+
+TEMPDIR = pathlib.Path(tempfile.mkdtemp())
+T_SLEEP = 10
+
+
+def kill_sleeper(
+    run_dir: pathlib.Path,
+    time: datetime.datetime,
+    lexical_path: str,
+    status: pwf.schemas.RunStatus,
+):
+    hard_coded_sleepy_path = "three_step.sleepy_0"
+    if (
+        lexical_path == hard_coded_sleepy_path
+        and status == pwf.schemas.RunStatus.RUNNING
+    ):
+        time.sleep(0.5)  # give the job a second to process
+        os._exit(0)  # Then hard exit so that we don't even wait for the executor
+
+
+def get_queue() -> list[dict[str, str]]:
+    """Return current squeue entries as list of dicts."""
+    cmd = ["squeue", "--noheader", "--format=%i|%j|%T|%u"]
+    result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    jobs = []
+    for line in result.stdout.splitlines():
+        job_id, name, state, job_user = line.split("|")
+        jobs.append({"job_id": job_id, "name": name, "state": state, "user": job_user})
+    return jobs
+
+
+def print_queue() -> None:
+    cmd = ["squeue"]
+    subprocess.run(cmd, check=True)
+
+
+def assert_queue_has_n_items(n: int) -> None:
+    jobs = get_queue()
+    assert len(jobs) == n, f"Expected {n} job(s) in queue, found {len(jobs)}: {jobs}"
+
+
+def setup():
+    submission_template = """\
+    #!/bin/bash
+    #SBATCH --output=time.out
+    #SBATCH --job-name={{job_name}}
+    #SBATCH --chdir={{working_directory}}
+    #SBATCH --get-user-env=L
+    #SBATCH --cpus-per-task={{cores}}
+
+    {{command}}
+    """
+    resource_dict = {"submission_template": submission_template}
+
+    wf = three_step.pwf.node()
+
+    wf.n2.executor = pwf.ExecutorInstructions(
+        pwf.tools.NodeSlurmExecutor,
+        (),
+        {"resource_dict": resource_dict},
+    )
+
+    cfg = pwf.RunConfig(run_dir=TEMPDIR, progress_hooks=[kill_sleeper])
+    return wf, cfg
+
+
+def submission():
+    wf, cfg = setup()
+    wf.run(cfg, t=T_SLEEP)
+
+
+def interruption():
+    print_queue()
+    assert_queue_has_n_items(1)
+    wf, cfg = setup()
+    t0 = time.time()
+    out = wf.run(cfg, t=T_SLEEP)
+    dt = time.time() - t0
+    assert_queue_has_n_items(0)
+    assert (
+        dt > 0.5 * T_SLEEP
+    ), f"Expected to need to wait for the job to finish, but got a small dt -- {dt}"
+    assert (
+        out.outputs["n3"].value == T_SLEEP
+    ), f"Sanity check that the workflow returned T_SLEEP, but got {out.outputs['n3'].value}"
+
+
+def discovery():
+    print_queue()
+    assert_queue_has_n_items(1)
+    time.sleep(1.5 * T_SLEEP)  # Wait for it to finish
+    assert_queue_has_n_items(0)
+    wf, cfg = setup()
+    t0 = time.time()
+    out = wf.run(cfg, t=T_SLEEP)
+    dt = time.time() - t0
+    assert (
+        dt < 0.1 * T_SLEEP
+    ), f"Expected to get a quick cache hit, but got a large -- {dt}"
+    assert (
+        out.outputs["n3"].value == T_SLEEP
+    ), f"Sanity check that the workflow returned T_SLEEP, but got {out.outputs['n3'].value}"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run workflow stages.")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--submit", action="store_true", help="Run submission stage.")
+    group.add_argument(
+        "--interrupt", action="store_true", help="Run interruption stage."
+    )
+    group.add_argument("--discover", action="store_true", help="Run discovery stage.")
+    args = parser.parse_args()
+
+    if args.submit:
+        submission()
+    elif args.interrupt:
+        interruption()
+    elif args.discover:
+        discovery()
+
+
+if __name__ == "__main__":
+    main()
