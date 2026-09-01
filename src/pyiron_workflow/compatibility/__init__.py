5808e6d8a78f4c6415daf4559cafe03eaf0b94fb
liamhuber <liamhuber@greyhavensolutions.com>
2026-09-01 09:24:24 -0700

    Re-introduce compatibility decorators
    From v0.18.0, pulling out just the minimal files to get the compatibility decorators working
    
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

---
+++ src/pyiron_workflow/compatibility/__init__.py
@@ -0,0 +1,3 @@
+from .decorators import as_function_node as as_function_node
+from .decorators import as_macro_node as as_macro_node
+from .multiple_dispatch import MultipleDispatchError as MultipleDispatchError
